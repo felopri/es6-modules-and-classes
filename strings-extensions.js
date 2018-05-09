dd47fd85a69618c985536cfb4086a7a3c3ffb929
Fernando <felopri@gmail.com>
2018-05-09 12:56:06 +0200

    String escape character

--- strings-extensions.js
+++ strings-extensions.js
@@ -1,5 +1,8 @@
-let title = 'Madrid Rio.';
+let title = 'Madrid \u{1f3c4} Rio';
 console.log(title.startsWith('Madrid'));
 //true
 console.log(title.endsWith('Rio'));
 //false
+
+console.log(title);
+//Madrid 🏄 Rio
