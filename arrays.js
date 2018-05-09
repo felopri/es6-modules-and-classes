b1c4012ce4ec15290a1b56220e8ea0bb977ac56b
Fernando <felopri@gmail.com>
2018-05-09 16:56:58 +0200

    Array from mehod creates a new calculated array

--- arrays.js
+++ arrays.js
@@ -6,3 +6,7 @@ let salaries2 = Array.of(90000);
 console.log(salaries2.length);
 //1
 
+let amounts = [800, 810, 820];
+let salaries3 = Array.from(amounts, v => v+100);
+console.log(salaries3);
+//[900, 910, 920]
