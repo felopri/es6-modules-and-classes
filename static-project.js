48a1a9899720bf537bc83fcc03e65da402e4fd81
Fernando <felopri@gmail.com>
2018-05-09 10:45:48 +0200

    Static methods and properties

--- static-project.js
+++ static-project.js
@@ -5,6 +5,11 @@ class Project {
 }
 console.log(Project.getDefaultId());
 //7
+
 let p = new Project();
 console.log(p.getDefaultId());
 //Error: Object doesn't support getDefaultId
+
+Project.id=99;
+console.log(Project.id);
+//99
