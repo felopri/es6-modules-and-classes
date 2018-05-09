c9c51ad1b39dd618bf7f18048bc6fa71b926f53e
Fernando <felopri@gmail.com>
2018-05-09 12:09:09 +0200

    Symbol os concat spreadable

--- blog.js
+++ blog.js
@@ -4,3 +4,9 @@ Blog.prototype[Symbol.toStringTag] = 'Blog Class';
 let blog = new Blog();
 console.log(blog.toString());
 //[object Blog Class]
+
+
+let values = [8, 12, 16];
+values[Symbol.isConcatSpreadable] = false;
+console.log([].concat(values));
+//[[8, 12, 16]] 
