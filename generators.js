45ea7175d4d740844e2103f64fa192f05b765c69
Fernando <felopri@gmail.com>
2018-05-09 14:22:39 +0200

    Generator function with yield

--- generators.js
+++ generators.js
@@ -3,6 +3,9 @@ function* process() {
     yield 8001;
 }
 let it = process();
-it.next();
+console.log(it.next());
+//{value: 8000, done: false}
 console.log(it.next());
 //{value: 8001, done: false}
+console.log(it.next());
+//{value: undefined, done: true}
