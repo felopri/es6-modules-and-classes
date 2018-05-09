3725a0385c8644b67dcabda9dca238a3f03fbe89
Fernando <felopri@gmail.com>
2018-05-09 12:12:36 +0200

    Symbol to primitive

--- blog.js
+++ blog.js
@@ -10,3 +10,12 @@ let values = [8, 12, 16];
 values[Symbol.isConcatSpreadable] = false;
 console.log([].concat(values));
 //[[8, 12, 16]] 
+
+values[Symbol.toPrimitive] = function (hint) {
+    console.log(hint);
+    return 36;
+}
+let sum = values + 100;
+console.log(sum);
+//default
+//142
