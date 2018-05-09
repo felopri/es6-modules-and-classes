44006531091e6d02b2a0f7214e646c8f7ab00feb
Fernando <felopri@gmail.com>
2018-05-09 13:58:10 +0200

    Function name

--- function-extension.js
+++ function-extension.js
@@ -1,5 +1,10 @@
-let fn = function calc() {
-    return 0;
-}
-console.log(fn.name);
+let func = function calc() {return 0;}
+console.log(func.name);
 //calc
+
+let fn = function () {return 0;}
+console.log(fn.name);
+//fn
+let newFn = fn;
+console.log(newFn.name);
+//fn
