af7ae8fe21771ade43b2437a39556850b4147449
Fernando <felopri@gmail.com>
2018-05-09 11:16:01 +0200

    Symbol as property

--- symbol.js
+++ symbol.js
@@ -1,3 +1,18 @@
 const CALCULATE_EVENT_SYMBOL = Symbol.for('calculate event');
 let description = Symbol.keyFor(CALCULATE_EVENT_SYMBOL);
 console.log(description);
+//calculate event
+
+let article = {
+    title: 'T3chFest 2018',
+    [Symbol.for('article')]: 'My article'
+}
+let value = article[Symbol.for('article')];
+console.log(value);
+//My article
+
+console.log(Object.getOwnPropertyNames(article));
+//['title']
+
+console.log(Object.getOwnPropertySymbols(article));
+//[Symbol(article)]
