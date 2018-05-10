6fe6614fa21dc01f9647dbf6d2eae147fbd8d8e9
Fernando <felopri@gmail.com>
2018-05-10 09:32:00 +0200

    Extending Array adding a method

--- subclassing.js
+++ subclassing.js
@@ -1,5 +1,17 @@
-class Perks extends Array {
+class ExtArray extends Array {
 }
-let a = Perks.from([5, 10, 15]);
-console.log(a instanceof Perks);
+let a = ExtArray.from([5, 10, 15]);
+console.log(a instanceof ExtArray);
 //true
+
+
+class Perks extends Array {
+    sum() {
+        let total=0;
+        this.map(v=>total+=v);
+        return total;
+    }
+}
+let b=Perks.from([5,10,15]);
+console.log(b.sum());
+//30
