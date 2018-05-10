10905714b462b5b4495ddc13c6513638efae77d0
Fernando <felopri@gmail.com>
2018-05-10 12:39:49 +0200

    Proxy revoke avoid access to an object

--- proxy.js
+++ proxy.js
@@ -25,3 +25,15 @@ console.log(p2.name);
 //Milton Waddams
 
 
+//----------REVOKE-----------------
+var t = { tableId: 99};
+let { proxy, revoke } = Proxy.revocable( t, {
+    get: function (target,prop,receiver) {
+        return Reflect.get(target,prop,receiver)+100;
+    }
+});
+console.log(proxy.tableId);
+//199
+revoke();
+console.log(proxy.tableId);
+//TypeError: Cannot perform 'get' on a proxy that has been revoked
