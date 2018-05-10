45ef08e330d994e0be293067c037110a9ea6edfa
Fernando <felopri@gmail.com>
2018-05-10 10:08:45 +0200

    Reflect set third argument refers to this object

--- reflect.js
+++ reflect.js
@@ -9,3 +9,22 @@ class Restaurant{
 let r = new Restaurant();
 console.log(Reflect.get(r, 'id', {_id:88}));
 //88
+
+
+
+class RestaurantTwo{
+    constructor(){
+        this._id = 900;
+    }
+    set id(value){
+        this._id = value;
+    }
+}
+let r2 = new RestaurantTwo();
+let alt = {id: 88};
+Reflect.set(r2, 'id', 88, alt);
+console.log(r2._id);
+//900
+console.log(alt._id);
+//88
+
